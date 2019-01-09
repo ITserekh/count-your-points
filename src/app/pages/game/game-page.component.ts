ba702ba8c9a863fb1be336ccbd6e0cf471c0e640
tserekhivan <tserekh.ivan@gmail.com>
2019-01-09 13:49:44 +0300

    feat: add game page

---
+++ src/app/pages/game/game-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'setting-page',
+  templateUrl: './game-page.component.html',
+  styleUrls: ['./game-page.component.css']
+})
+export class GamePage { }
