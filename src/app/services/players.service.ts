4bad8f8f8bc4a67f22eae82d8b553c731d730af2
tserekhivan <tserekh.ivan@gmail.com>
2019-01-11 11:40:56 +0300

    feat: add player service

---
+++ src/app/services/players.service.ts
@@ -0,0 +1,22 @@
+import {Player} from './player';
+ 
+export class PlayersService{
+ 
+    private players: Player[] = [
+      { name: 'Sherlock Holmes', eMail: 'sherlock.holmes@gmail.com' },
+      { name: 'Dr. John Watson', eMail: 'dr.john.watson@gmail.com' },
+      { name: 'Mrs. Stubs', eMail: 'stubbs@gmail.com' },
+      { name: 'Jim Moriarty', eMail: 'jim.moriarty@gmail.com' }
+    ];
+
+    getPlayers(): Player[] {
+      return this.players;
+    }
+
+    deletePlayer(player: Player) {
+      // find item index of players array for delete
+      let index: number = this.players.findIndex((item) => player === item);
+      // delete a one item with index from players list
+      this.players.splice(index, 1);
+    }
+}
