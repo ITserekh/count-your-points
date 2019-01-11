bbc3c47a7b9100019ad385da88a75271b45fee21
tserekhivan <tserekh.ivan@gmail.com>
2019-01-11 11:44:11 +0300

    refactor: edit app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app',
-  template: `<router-outlet></router-outlet>`
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   constructor() {
