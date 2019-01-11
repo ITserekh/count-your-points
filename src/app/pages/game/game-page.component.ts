700c87692bae33a04e69438dc7d89dcdb2bcab8a
tserekhivan <tserekh.ivan@gmail.com>
2019-01-11 11:45:59 +0300

    refactor: edit game page component

--- src/app/pages/game/game-page.component.ts
+++ src/app/pages/game/game-page.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'setting-page',
   templateUrl: './game-page.component.html',
-  styleUrls: ['./game-page.component.css']
+  styleUrls: ['./game-page.component.scss']
 })
 export class GamePage { }
