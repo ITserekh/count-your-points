8b8826a4037e1445ce688dcddeefb1af3cd2bd48
tserekhivan <tserekh.ivan@gmail.com>
2019-01-13 23:10:36 +0300

    feat: add show points and show winners components

---
+++ src/app/components/show-points/show-points.component.ts
@@ -0,0 +1,29 @@
+import { Component, Inject } from '@angular/core';
+import {MAT_DIALOG_DATA} from '@angular/material';
+
+import { Throw } from '../../services/throw';
+import { ThrowService } from '../../services/throw.service';
+import { Player } from '../../services/player';
+import { PlayersService } from '../../services/players.service';
+
+@Component({
+  selector: 'show-points',
+  templateUrl: './show-points.component.html',
+  styleUrls: ['./show-points.component.scss']
+})
+export class ShowPointsComponent {
+  moveNumber: number; // number of selected move
+  players: Player[];
+  throws: Throw[][];
+
+  constructor(private playersService: PlayersService,
+    private throwService: ThrowService,
+    @Inject(MAT_DIALOG_DATA) public data: any) {
+      this.moveNumber = data.moveNumber;
+    }
+
+  ngOnInit() {
+    this.players = this.playersService.getPlayers();
+    this.throws = this.throwService.getThrows();
+  }
+}
